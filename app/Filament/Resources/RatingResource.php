ae0e7a5a19886e8f54d964191ffe2d10f9ab5157
JauzaNaufal <541231095@student.smktelkom-pwt.sch.id>
2024-12-09 08:16:26 +0700

    Laravel

--- app/Filament/Resources/RatingResource.php
+++ app/Filament/Resources/RatingResource.php
@@ -60,6 +60,8 @@ class RatingResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
